a0ef8f0c6965cf0e93fabb95454b4e910a5e11b5
virkonomy <arif@tokenomy.com>
2019-09-04 18:50:49 +0700

    match idk GSM

--- lib/idk_pay_web/controllers/page_controller.ex
+++ lib/idk_pay_web/controllers/page_controller.ex
@@ -6,65 +6,77 @@ defmodule IdkPayWeb.PageController do
 
   def index(conn, _params) do
     conn
-    |> IO.inspect
     |> render("index.html", layout: {IdkPayWeb.LayoutView, "fe.html"})
   end
 
+  def dashboard(conn, _params) do
+    conn
+    |> render("dashboard.html", layout: {IdkPayWeb.LayoutView, "fe.html"})
+  end
+
+  def integration(conn, _params) do
+    conn
+    |> render("integration.html", layout: {IdkPayWeb.LayoutView, "fe.html"})
+  end
+
+  def pricing(conn, _params) do
+    conn
+    |> render("pricing.html", layout: {IdkPayWeb.LayoutView, "fe.html"})
+  end
+
   def login(conn, _params) do
-  	changeset = Account.change_user(%User{})
+    changeset = Account.change_user(%User{})
     render(conn, "login.html", layout: {IdkPayWeb.LayoutView, "fe.html"}, changeset: changeset)
   end
 
-  def lgn(conn, _params) do
-  	changeset = Account.change_user(%User{})
-    render(conn, "login-nosocial.html", layout: {IdkPayWeb.LayoutView, "fe.html"}, changeset: changeset)
-  end
-
   def register(conn, _params) do
-  	changeset = Account.change_user(%User{})
+    changeset = Account.change_user(%User{})
     render(conn, "register.html", layout: {IdkPayWeb.LayoutView, "fe.html"}, changeset: changeset)
   end
 
   def createuser(conn, %{"user" => params}) do
-  	changeset = Account.change_user(%User{})
     case Account.create_user_frontend(params) do
-      {:ok, user} ->
+      {:ok, _user} ->
         conn
         |> put_flash(:info, "User created successfully. You can login now")
-        |> redirect(to: "/lgn")
+        |> redirect(to: "/login")
+
       {:error, %Ecto.Changeset{} = changeset} ->
-      	IO.inspect(changeset)
-      	conn
-      	|> put_flash(:error, "Oops, check error below")
+        IO.inspect(changeset)
+
+        conn
+        |> put_flash(:error, "Oops, check error below")
         |> render("register.html", layout: {IdkPayWeb.LayoutView, "fe.html"}, changeset: changeset)
     end
   end
 
-
-
   def auth(conn, %{"email" => email, "password" => password}) do
-  case Account.authenticate_user_front(email, password) do
-    {:ok, user} ->
-      conn
-      |> put_session(:current_user_id, user.id)
-      |> redirect(to: "/")
-    {:error, reason} ->
-      changeset = Account.change_user(%User{})
-      conn
-      |> put_flash(:error, reason)
-      |> render("login-nosocial.html", layout: {IdkPayWeb.LayoutView, "fe.html"}, changeset: changeset)
+    case Account.authenticate_user_front(email, password) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> redirect(to: "/")
+
+      {:error, reason} ->
+        changeset = Account.change_user(%User{})
+
+        conn
+        |> put_flash(:error, reason)
+        |> render("login-nosocial.html",
+          layout: {IdkPayWeb.LayoutView, "fe.html"},
+          changeset: changeset
+        )
     end
   end
 
-
   def createuser(conn, params) do
-	IO.inspect(params)
-	text(conn, "mbel")  	
+    IO.inspect(params)
+    text(conn, "mbel")
   end
 
   def recover(conn, _params) do
     render(conn, "recover.html", layout: {IdkPayWeb.LayoutView, "fe.html"})
-  end  
+  end
 
   def signout(conn, _parms) do
     conn
--- lib/idk_pay_web/router.ex
+++ lib/idk_pay_web/router.ex
@@ -4,56 +4,57 @@ defmodule IdkPayWeb.Router do
   # -----------------pipeline ----------------
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-    plug IdkPayWeb.Plugs.SetCurrentUser
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(IdkPayWeb.Plugs.SetCurrentUser)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   pipeline :auth do
     plug(IdkPay.Auth.AuthAccessPipeline)
   end
 
-
   # ----------------- scope route ----------------
 
-
   scope "/admin", IdkPayWeb do
-    pipe_through([:browser,:auth]) # Use the default browser stack
+    # Use the default browser stack
+    pipe_through([:browser, :auth])
 
-    get "/", UserController, :dashboard
-    get "/profile", UserController, :profile
-    get "/locked", UserController, :locked
-    resources "/activity", ActivityController, only: [:index, :show, :delete]
-    resources "/user", UserController
-    resources "/invoices", InvoiceController
-    resources "/addresses", AddressController
-    resources "/transactions", TransactionController
-    get "/logout", UserController, :logout 
+    get("/", UserController, :dashboard)
+    get("/profile", UserController, :profile)
+    get("/locked", UserController, :locked)
+    resources("/activity", ActivityController, only: [:index, :show, :delete])
+    resources("/user", UserController)
+    resources("/invoices", InvoiceController)
+    resources("/addresses", AddressController)
+    resources("/transactions", TransactionController)
+    get("/logout", UserController, :logout)
   end
-
 
   scope "/", IdkPayWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
-    get "/login", PageController, :login
-    get "/lgn", PageController, :lgn    
-    get "/register", PageController, :register
-    post "/login", PageController, :auth
-    post "/register", PageController, :createuser 
-    get "/recover", PageController, :recover        
-    get "/admin/login", UserController, :login
-    post "/admin/login", UserController, :auth  
-    get "/signout", PageController, :signout 
+    get("/dashboard", PageController, :dashboard)
+
+    get("/", PageController, :index)
+    get("/login", PageController, :login)
+    get("/lgn", PageController, :lgn)
+    get("/integration", PageController, :integration)
+    get("/pricing", PageController, :pricing)
+    get("/register", PageController, :register)
+    post("/login", PageController, :auth)
+    post("/register", PageController, :createuser)
+    get("/recover", PageController, :recover)
+    get("/admin/login", UserController, :login)
+    post("/admin/login", UserController, :auth)
+    get("/signout", PageController, :signout)
   end
-
 
   # Other scopes may use custom stacks.
   # scope "/api", IdkPayWeb do
