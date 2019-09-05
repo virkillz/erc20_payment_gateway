f7a916be1f76c3d9037c0dcfac887804843a6ba4
virkonomy <arif@tokenomy.com>
2019-09-06 06:34:49 +0700

    Refactor ethereum address model

--- lib/idk_pay/ethereum/address.ex
+++ lib/idk_pay/ethereum/address.ex
@@ -2,12 +2,13 @@ defmodule IdkPay.Ethereum.Address do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "addresses" do
-    field :addr, :string
-    field :remark, :string
-    field :user_id, :id
-    field :invoice_id, :id
+    field(:addr, :string)
+    field(:remark, :string)
+    field(:invoice_id, :id)
+    field(:is_used, :boolean)
+    field(:network, :string)
+    field(:privkey, :string)
 
     timestamps()
   end
