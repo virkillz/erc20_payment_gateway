ec044297ab222da81be62315591f1f273332ce9c
virkonomy <arif@tokenomy.com>
2019-09-06 06:35:11 +0700

    refactor invoice model

--- lib/idk_pay/transaction/invoice.ex
+++ lib/idk_pay/transaction/invoice.ex
@@ -2,17 +2,20 @@ defmodule IdkPay.Transaction.Invoice do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "invoices" do
-    field :amount, :integer
-    field :expiry_date, :date
-    field :fee, :integer
-    field :invoice_status, :string
-    field :payment_status, :string
-    field :purpose, :string
-    field :token, :string
-    field :total, :integer
-    field :user_id, :id
+    field(:amount, :integer)
+    field(:invoice_status, :string)
+    field(:payment_status, :string)
+    field(:purpose, :string)
+    field(:token, :string)
+    field(:user_id, :id)
+    field(:eth_address, :string)
+    field(:is_settled, :boolean)
+    field(:settlement_fee, :integer)
+    field(:settlement_nett, :integer)
+    field(:expiry, :string)
+    field(:expiry_date, :naive_datetime)
+    field(:success_redirect, :string)
 
     timestamps()
   end
@@ -20,7 +23,24 @@ defmodule IdkPay.Transaction.Invoice do
   @doc false
   def changeset(invoice, attrs) do
     invoice
-    |> cast(attrs, [:token, :amount, :fee, :total, :payment_status, :invoice_status, :expiry_date, :purpose])
-    |> validate_required([:token, :amount, :fee, :total, :payment_status, :invoice_status, :expiry_date, :purpose])
+    |> cast(attrs, [
+      :token,
+      :amount,
+      :payment_status,
+      :invoice_status,
+      :expiry_date,
+      :expiry,
+      :eth_address,
+      :is_settled,
+      :purpose
+    ])
+    |> validate_required([
+      :token,
+      :amount,
+      :payment_status,
+      :invoice_status,
+      :expiry_date,
+      :purpose
+    ])
   end
 end
