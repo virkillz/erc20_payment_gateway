a544e25190427c1edaaac16847e6d2d8b7693818
virkonomy <arif@tokenomy.com>
2019-09-06 06:35:40 +0700

    change invoice migration

--- priv/repo/migrations/20190723085513_create_invoices.exs
+++ priv/repo/migrations/20190723085513_create_invoices.exs
@@ -3,19 +3,23 @@ defmodule IdkPay.Repo.Migrations.CreateInvoices do
 
   def change do
     create table(:invoices) do
-      add :token, :string
-      add :amount, :integer
-      add :fee, :integer
-      add :total, :integer
-      add :payment_status, :string
-      add :invoice_status, :string
-      add :expiry_date, :date
-      add :purpose, :text
-      add :user_id, references(:user, on_delete: :nothing)
+      add(:token, :string)
+      add(:amount, :bigserial)
+      add(:payment_status, :string)
+      add(:invoice_status, :string)
+      add(:eth_address, :string)
+      add(:is_settled, :boolean)
+      add(:settlement_fee, :bigserial)
+      add(:settlement_nett, :bigserial)
+      add(:expiry, :string)
+      add(:expiry_date, :naive_datetime)
+      add(:success_redirect, :string)
+      add(:purpose, :text)
+      add(:user_id, references(:user, on_delete: :nothing))
 
       timestamps()
     end
 
-    create index(:invoices, [:user_id])
+    create(index(:invoices, [:user_id]))
   end
 end
