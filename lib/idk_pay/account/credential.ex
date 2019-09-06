96f66da491121756e6eeb3afac25fc63c69e8b5d
virkonomy <arif@tokenomy.com>
2019-09-06 17:19:28 +0700

    Refactor credentials changeset

--- lib/idk_pay/account/credential.ex
+++ lib/idk_pay/account/credential.ex
@@ -3,14 +3,14 @@ defmodule IdkPay.Account.Credential do
   import Ecto.Changeset
 
   schema "credentials" do
-    field :eth_address, :string
-    field :is_deleted, :boolean, default: false
-    field :network, :string
-    field :public_key, :string
-    field :secret_key, :string
-    field :success_url_redirect, :string
-    field :webhook_url, :string
-    field :user_id, :id
+    field(:eth_address, :string)
+    field(:is_deleted, :boolean, default: false)
+    field(:network, :string)
+    field(:public_key, :string)
+    field(:secret_key, :string)
+    field(:success_url_redirect, :string)
+    field(:webhook_url, :string)
+    field(:user_id, :id)
 
     timestamps()
   end
@@ -18,7 +18,16 @@ defmodule IdkPay.Account.Credential do
   @doc false
   def changeset(credential, attrs) do
     credential
-    |> cast(attrs, [:eth_address, :webhook_url, :success_url_redirect, :public_key, :secret_key, :network, :is_deleted])
-    |> validate_required([:eth_address, :webhook_url, :success_url_redirect, :public_key, :secret_key, :network, :is_deleted])
+    |> cast(attrs, [
+      :eth_address,
+      :webhook_url,
+      :user_id,
+      :success_url_redirect,
+      :public_key,
+      :secret_key,
+      :network,
+      :is_deleted
+    ])
+    |> validate_required([:eth_address, :webhook_url, :success_url_redirect, :network])
   end
 end
