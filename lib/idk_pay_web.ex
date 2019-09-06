993387003b6f34e750973d5099b862e3e1227ca0
virkonomy <arif@tokenomy.com>
2019-09-06 12:51:04 +0700

    Fix router liveview compatibility

--- lib/idk_pay_web.ex
+++ lib/idk_pay_web.ex
@@ -54,7 +54,7 @@ defmodule IdkPayWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
-      import Phoenix.LiveView, only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2]
+      import Phoenix.LiveView.Router
     end
   end
 
